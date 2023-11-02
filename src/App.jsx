511bf02dfa4c0cd0c5dcf28f1ad63ced0c356673
iamtonmoy0 <tonmoy2code@gmail.com>
2023-11-02 10:27:20 -0400

    style:grid layout added

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import Gallery from "./components/Gallery/Gallery"
 function App() {
 
   return (
-    <div className="bg-slate-200 h-screen">
+    <>
     <Gallery/>
-    </div>
+    </>
     )
 }
 
--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,8 +1,29 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 
 const Gallery = () => {
+	const [image,setImage]=useState([]);
+	useEffect(()=>{
+		fetch('/data.json') // fetching data from json
+		.then((response) => response.json())
+		.then((data) => {
+		setImage(data.images);
+		})
+
+	},[setImage])
+	console.log(image)
 	return (
-		<div>
-			Gallery Page
+		<div className="bg-white w-11/12 mx-auto  rounded-md h-auto mt-14">
+			<hr />
+			<div className="bg-red w-11/12 mx-auto pt-5 " id="gallery">
+			{image.map((img,index)=>
+			<div key={img.id} className={`item-no-${index+1}`} id="single-image">
+				<img  src={img.src}/>
+			</div>
+			)}
+			</div>
+
 		</div>
 	);
 }
